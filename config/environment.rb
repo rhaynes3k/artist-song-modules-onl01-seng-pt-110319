c2afcbdf3719cb6124e8968a579a57459d0fd7b5
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-05 02:19:47 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 require 'pry'
-Bundler.require
-require_all 'lib'
+
+
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/paramable'
 
-# require_relative '../lib/artist.rb'
-# require_relative '../lib/song.rb'
+require_relative '../lib/artist.rb'
+require_relative '../lib/song.rb'
